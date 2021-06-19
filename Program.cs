f453398f670008a9c99f59abcdad7c13f0e0c992
Tatiane Pedrelli <tatiane@tatianepedrelli.com.br>
2021-06-19 20:59:39 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Recursao
+{
+    class Program
+    {
+        public static int Fatorial(int n)
+        {
+            if ((n == 0) || (n == 1))
+            {
+                return 1;
+            }
+            else return (n * Fatorial(n -1));
+        }
+
+        public static void Main()
+        {
+            string input;
+            int num, result;
+            Console.WriteLine("Este programa calcula o fatorial do número que você informar.");
+            Console.WriteLine("Informe um número");
+            input = Console.ReadLine();
+            num = Convert.ToInt32(input);
+            result = Fatorial(num);
+            Console.WriteLine("O fatorial de {0} é: {1}", num, result);
+        }
+    }
+}
